67e793fbaf645b46ce4f7d86c6e055f4595ba53a
Jasurbek Toshev <jasurbektoshev20@gmail.com>
2026-03-31 12:09:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,48 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import asyncio
 import os
+import sqlite3
 
+conn = sqlite3.connect("groups.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS groups (
+    id INTEGER PRIMARY KEY
+)
+""")
+conn.commit()
 TOKEN = os.getenv("BOT_TOKEN")
 
-GROUPS = set()
+# GROUPS = set()
 
 async def main_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     # Guruhni saqlash
+    # Guruhni saqlash
     if update.message:
         chat = update.message.chat
+
         if chat.type in ["group", "supergroup"]:
-            GROUPS.add(chat.id)
-            print("SAQLANDI:", chat.id)
+            cursor.execute("SELECT id FROM groups WHERE id=?", (chat.id,))
+            result = cursor.fetchone()
+
+            if not result:
+                cursor.execute("INSERT INTO groups (id) VALUES (?)", (chat.id,))
+                conn.commit()
+                print("BAZAGA SAQLANDI:", chat.id)
 
     # Kanal post
     if update.channel_post:
         msg = update.channel_post
         print("POST KELDI")
 
-        for group_id in GROUPS:
+        cursor.execute("SELECT id FROM groups")
+        groups = cursor.fetchall()
+
+        for group in groups:
+            group_id = group[0]
+
             try:
                 await context.bot.forward_message(
                     chat_id=group_id,
@@ -32,9 +54,20 @@ async def main_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 await asyncio.sleep(1)
             except Exception as e:
                 print("XATO:", e)
+                # 🔥 agar group ishlamasa o‘chiramiz
+                cursor.execute("DELETE FROM groups WHERE id=?", (group_id,))
+                conn.commit()
+                print("O‘CHIRILDI:", group_id)
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, main_handler))
 
 print("Bot ishga tushdi...")
 app.run_polling()
+
+import atexit
+
+def close_db():
+    conn.close()
+
+atexit.register(close_db)
